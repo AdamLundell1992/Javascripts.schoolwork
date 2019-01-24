be32f8cc19b2531b4478633b2e913634a35e55bd
AdamLundell1992 <Owned122>
2019-01-24 10:34:48 +0000

    test

--- src/week2_2019/Diary.java
+++ src/week2_2019/Diary.java
@@ -1,28 +1,37 @@
 package week2_2019;
+import javafx.scene.layout.Priority;
+
 import java.time.format.SignStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+
+import static week2_2019.Job.*;
 
 /**
  * Created by u1853373 on 22/01/2019.
  */
 public class Diary {
-
+// Arraylist for my jobs, i will create them in my main method in the bottom on this class.
     private ArrayList <Job> jobs = new ArrayList<>();
 
     public Diary() {
     }
-
+//Arraylist for the newjobs i create- this method will add the jobs to the Arraylist.
     public void addJob(Job newJob ) {
         this.jobs.add(newJob);
     }
-    public void printJobs(){
-        for (Job job : jobs){
-            if(!job.isCompleted()){
+    public void printJobs() {
+        for (Job job : jobs) {
+            if (!job.isCompleted()) {
                 System.out.println(job);
+
             }
         }
     }
+// Arraylist for completed Jobs if the job is completed it will be added to a new Arraylist,
+// that i can use in my mainmethood by typing .printCompletedJobs.
+// i need to choose my self if the job are completed inside the main method with set.completed.
     public void printCompletedJobs(){
         for (Job job : jobs){
             if(job.isCompleted()){
@@ -32,9 +41,48 @@ public class Diary {
     }
 
 
+    //public void isFound(String word){
+    //for (Job job : jobs){
+     //   if (job.isFound()){
+         //   System.out.println(job);
+       // }
+   // }
+//}
+
+
+
+
+//Sort team is just a name... This method will sort my jobs , in this case with
+// the priority it will sort it from 1-5(1bottom 5 top).Need to add the sortteam inside tha main
+// to put in in use
+
     public void sortTeam () {
         Collections.sort (this.jobs);
-    }
+
+   }
+// This Arraylists name is "returnList" And the if inside means that if the jobname
+// cointains the word you type in (lowercase will just make so the word you type in can be lover)
+// If the word "or the letter" is found the "jobname" arraylist- "returnList" will add the
+// return the job.
+   public ArrayList<Job> searchDiary(String word){
+       ArrayList<Job> returnList = new ArrayList<>(0);
+       boolean found = false;
+       for (Job job : jobs){
+           if(job.getDescription().toLowerCase().contains(word.toLowerCase())){
+               //System.out.println(job.getDescription() + " contains " + word);
+               returnList.add(job);
+               found = true;
+           }
+       }
+       // if the returnList is empty it will return null else it will return the Arraylist
+       // called "returnList"
+       if(returnList.isEmpty()){
+           return null;
+       }else{
+           return returnList;
+       }
+
+   }
 
 
     public static void main(String[] args) {
@@ -44,6 +92,7 @@ public class Diary {
         Job j3 = new Job("Security guard", 3);
         Job j2 = new Job("Gym Staff", 2);
         Job j4 = new Job("Programmer", 4);
+        Job j6 = new Job("Gym Trainer", 2);
         final String formatString = "%-8s %15s %12s";
         //here i printed out the names on the board.over the numbers
         System.out.println( (String.format (formatString,
@@ -51,13 +100,17 @@ public class Diary {
                 "Priority",
                 "Completed")));
 
+        System.out.println("");
         j1.setCompleted();
         diary.addJob(j1);
         diary.addJob(j2);
         diary.addJob(j3);
         diary.addJob(j4);
         diary.addJob(j5);
+        diary.addJob(j6);
         diary.sortTeam();
+
+
         diary.printJobs();
         System.out.println("");
         System.out.println("");
@@ -68,7 +121,36 @@ public class Diary {
                 "description",
                 "Priority",
                 "Completed")));
+        System.out.println("");
         diary.printCompletedJobs();
+        // "The scanner class" it reads a string and search for the word.
+        System.out.println("Enter a word: ");
+        Scanner scanner = new Scanner(System.in);
+        String searchWord = scanner.nextLine();
+        // returnList search for the word and if the returnList != not= null #
+        // it will loop through the diary to find a simular "word letter" inside the
+        // diary and print out the job.
+        ArrayList<Job> returnList = diary.searchDiary(searchWord);
+        if(returnList != null){
+            for(Job job : diary.searchDiary(searchWord)){
+                System.out.println(job);
+            }
+            // Else it will print this "You search term did not find a match"
+        }else{
+            System.out.println("You search term did not find a match");
+        }
+
+
+
     }
 }
+
+        //String stringToTest = Job.getDescription();
+
+       // boolean isFound = stringToTest.contains(word);
+
+
+
+
+
 
--- src/week2_2019/Job.java
+++ src/week2_2019/Job.java
@@ -3,30 +3,39 @@ package week2_2019;
 /**
  * Created by u1853373 on 22/01/2019.
  */
+// Class name and implements Comparable i use to sort the Priority list.look down in this code
 public class Job implements Comparable <Job> {
 
-
+// Attributes
     private String description;
     private int priority;
-
-
-
     private boolean completed;
+    private boolean found;
+
+
 
 
     public Job(String description, int priority) {
         this.description = description;
         this.priority = priority;
         this.completed = false;
+        this.found = false;
     }
 
+/// getters and setters
+    public  String getDescription() {
 
-    public String getDescription() {
         return description;
     }
 
+    public boolean isFound() {
+        return isFound();
+    }
 
-    public int getPriority() {
+    public void setFound(boolean found) {
+        this.found = found;
+    }
+    public  int getPriority() {
         return priority;
     }
 
@@ -42,6 +51,9 @@ public class Job implements Comparable <Job> {
         return completed;
     }
 
+
+
+
     @Override
     public String toString() {
             final String formatString = "%-15s %5s %15s";
@@ -52,6 +64,9 @@ public class Job implements Comparable <Job> {
                 this.completed,
                 '}'));
     }
+//Here i use the Compare i have in my class name/ the boolean will compare my priority and
+// make it so it can be sorted in a list from 1-5 in my output when i make printJobs
+// it will print a list of the jobs and the priority and this will sort it.
 
     @Override
     public int compareTo(Job j) {
